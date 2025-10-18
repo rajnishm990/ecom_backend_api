1d895fddb256ca6297369a1d2c097405a7bb0ee5
rajnish <rajnishm990@gmail.com>
2025-10-18 20:26:17 +0530

    products: admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'in_stock', 'created_at']
+    list_filter = ['category', 'created_at']
+    search_fields = ['name', 'description']
+    list_editable = ['price', 'stock']  # Quick edit from list view
+    ordering = ['-created_at']
