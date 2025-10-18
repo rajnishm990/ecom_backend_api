16b55f386a841e18fe730ae6b610a55f4d14b141
rajnish <rajnishm990@gmail.com>
2025-10-18 15:38:16 +0530

    user:admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+# Register UserProfile in admin panel
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number', 'city', 'country', 'created_at']
+    search_fields = ['user__username', 'user__email', 'phone_number']
+    list_filter = ['country', 'created_at']
